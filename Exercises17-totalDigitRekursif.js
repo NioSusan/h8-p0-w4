1412dd2034710ea053940a9cedee92f99c59e978
Susan Nio <susan.wen88@gmail.com>
2018-06-23 00:58:24 +0700

    Refactoring Solution

--- Exercises17-totalDigitRekursif.js
+++ Exercises17-totalDigitRekursif.js
@@ -1,17 +1,29 @@
-function ubahAngkaToStr(num){
-    return String(num).split("");
-}
-  
+/* After Refactoring */
 function totalDigitRekursif(angka) {
-    var arrAngka= ubahAngkaToStr(angka);
-    if(arrAngka.length===1){
-      return Number(arrAngka);
-    } else {
-      var angka1 = Number(arrAngka.splice(0,1));
-      var sisanya= arrAngka.join("");
-      return (angka1) + totalDigitRekursif(sisanya);
-    }
+  var stringNum = String(angka);
+  if(stringNum.length===1){
+    return Number(stringNum);
+  } else {
+    return Number(stringNum[0]) + totalDigitRekursif((stringNum.slice(1)));
+  }
 }
+
+/* ========================================================================== */
+/* Before Refactoring */
+// function ubahAngkaToStr(num){
+//     return String(num).split("");
+// }
+  
+// function totalDigitRekursif(angka) {
+//     var arrAngka= ubahAngkaToStr(angka);
+//     if(arrAngka.length===1){
+//       return Number(arrAngka);
+//     } else {
+//       var angka1 = Number(arrAngka.splice(0,1));
+//       var sisanya= arrAngka.join("");
+//       return (angka1) + totalDigitRekursif(sisanya);
+//     }
+// }
   
 // TEST CASES
 console.log(totalDigitRekursif(512)); // 8
