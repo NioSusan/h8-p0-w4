92a3ef1ea26012df768054d63ee4ad27aefde34a
Susan Nio <susan.wen88@gmail.com>
2018-06-23 21:27:04 +0700

    Adding Alternative Solution

--- Exercises5-ubahHuruf.js
+++ Exercises5-ubahHuruf.js
@@ -1,8 +1,19 @@
+//Solution 1 - using regex
 function ubahHuruf(kata) {
     return kata.replace(/[a-zA-Z]/g, function(x){
       return String.fromCharCode(x.charCodeAt(0)+1);
     });
 }
+
+//Solution 2 - Using charCodeAt() and String.fromCharCode()
+// function ubahHuruf(kata) {
+//   var str="";
+//   for(var letter of kata){
+//     var num=letter.charCodeAt()+1;
+//     str+=String.fromCharCode(num);
+//   }
+//   return str;
+// }
   
 // TEST CASES
 console.log(ubahHuruf('wow')); // xpx
