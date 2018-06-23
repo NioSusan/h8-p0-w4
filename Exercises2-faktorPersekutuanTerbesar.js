93354963d51ebee359141430fd37d76b911ba481
Susan Nio <susan.wen88@gmail.com>
2018-06-23 22:27:03 +0700

    Adding Alternative Solution

--- Exercises2-faktorPersekutuanTerbesar.js
+++ Exercises2-faktorPersekutuanTerbesar.js
@@ -1,3 +1,41 @@
+//Solution 1 - using object
+function fpb(angka1, angka2) {
+  var arr=[];
+
+  for(var i=1;i<=angka1;i++){
+    if(angka1%i===0){
+      arr.push(i);
+    }
+  }
+
+  for(var j=1;j<=angka2;j++){
+    if(angka2%j===0){
+      arr.push(j);
+    }
+  }
+  
+  var obj={};
+  for(var num of arr){
+    // console.log(num);
+    if(!obj[num]){
+      obj[num]=0;
+    }
+    obj[num]++;
+  }
+  // console.log(obj);
+  var winnerNum=0;
+  var winnerName;
+  for(var key in obj){
+    if(obj[key]===2 && Number(key)>winnerNum){
+      winnerNum = Number(key);
+      winnerName = key;
+    }
+  }
+  return winnerName;
+}
+
+
+//Solution 2 - shorter 
 function fpb(angka1, angka2) {
     if ((typeof angka1 !== 'number') || (typeof angka2 !== 'number')){
       return false;
